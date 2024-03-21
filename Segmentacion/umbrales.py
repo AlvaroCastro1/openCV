ead61ba0374b9e35b95234d7dd957f6b12410815
AlvaroCastro1 <jesus.27castrop@gmail.com>
2024-03-21 17:59:36 -0600

    umbrales

--- Segmentacion/umbrales.py
+++ Segmentacion/umbrales.py
@@ -46,6 +46,28 @@ def umbral_porNivel_invertido(imagen, umbral_valor):
                 salida[i, j] = 0
     return salida
 
+def umbral_por2puntos(imagen, umbral_valor1, umbral_valor2):
+    alto, ancho = imagen.shape
+    salida = np.zeros_like(imagen)
+    for i in range(alto):
+        for j in range(ancho):
+            if imagen[i, j] <= umbral_valor1 and imagen[i, j] >= umbral_valor2:
+                salida[i, j] = 0
+            else:
+                salida[i, j] = imagen[i,j]
+    return salida
+
+def umbral_por2puntos_invertido(imagen, umbral_valor1, umbral_valor2):
+    alto, ancho = imagen.shape
+    salida = np.zeros_like(imagen)
+    for i in range(alto):
+        for j in range(ancho):
+            if imagen[i, j] <= umbral_valor1 and imagen[i, j] >= umbral_valor2:
+                salida[i, j] = imagen[i,j]
+            else:
+                salida[i, j] = 0
+    return salida
+
 if __name__ == "__main__":
     imagen = cv2.imread('c:/Users/Hp245-User/Desktop/openCV/images/amarilla.png')
     imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
@@ -57,28 +79,46 @@ if __name__ == "__main__":
     umb_inv_img = umbral_invertido(imagen, umbral_valor)
     um_nivel_img = umbral_porNivel(imagen, umbral_valor)
     um_nivel_inv_img = umbral_porNivel_invertido(imagen, umbral_valor)
+    um_2puntos_img = umbral_por2puntos(imagen, umbral_valor, umbral_valor+100)
+    um_2puntos_inv_img = umbral_por2puntos_invertido(imagen, umbral_valor, umbral_valor+100)
 
     import matplotlib.pyplot as plt    
     plt.figure(figsize=(10, 5))
 
-    plt.subplot(231)
+    plt.subplot(331)
     plt.imshow(cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB))
-    plt.title('Umbral')
-
-    plt.subplot(232)
+    plt.title('imagen')
+    
+    plt.subplot(332)
     plt.imshow(cv2.cvtColor(umb_img, cv2.COLOR_BGR2RGB))
-    plt.title('Umbral')
+    plt.title('imagen')
     
-    plt.subplot(233)
+    plt.subplot(333)
     plt.imshow(cv2.cvtColor(umb_inv_img, cv2.COLOR_BGR2RGB))
-    plt.title('Umbral inv')
+    plt.title('imagen')
     
-    plt.subplot(234)
+    plt.subplot(334)
     plt.imshow(cv2.cvtColor(um_nivel_img, cv2.COLOR_BGR2RGB))
-    plt.title('Umbral por nivel')
+    plt.title('imagen')
     
-    plt.subplot(235)
+    plt.subplot(335)
     plt.imshow(cv2.cvtColor(um_nivel_inv_img, cv2.COLOR_BGR2RGB))
-    plt.title('Umbral por nivel inv')
+    plt.title('imagen')
+    
+    plt.subplot(336)
+    plt.imshow(cv2.cvtColor(um_nivel_img, cv2.COLOR_BGR2RGB))
+    plt.title('imagen')
+    
+    plt.subplot(337)
+    plt.imshow(cv2.cvtColor(um_nivel_inv_img, cv2.COLOR_BGR2RGB))
+    plt.title('imagen')
 
+    plt.subplot(338)
+    plt.imshow(cv2.cvtColor(um_2puntos_img, cv2.COLOR_BGR2RGB))
+    plt.title('imagen')
+    
+    plt.subplot(339)
+    plt.imshow(cv2.cvtColor(um_2puntos_inv_img, cv2.COLOR_BGR2RGB))
+    plt.title('imagen')
+    
     plt.show()
