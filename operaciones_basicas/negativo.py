c20a519cd31e03c19a1da7889c4d1e0aac553608
AlvaroCastro1 <jesus.27castrop@gmail.com>
2024-03-25 20:36:01 -0600

    bn y color

--- operaciones_basicas/negativo.py
+++ operaciones_basicas/negativo.py
@@ -1,21 +1,28 @@
 import cv2
 
 def obtener_negativo(imagen):
-    alto, ancho, _ = imagen.shape
+    alto, ancho = imagen.shape[:2]
     
     negativo = imagen.copy()
     
-    for y in range(alto):
-        for x in range(ancho):
-            pixel = imagen[y, x]
-            # nuevo pixel en negativo (225 - canal)
-            negativo[y, x] = [255 - pixel[0], 255 - pixel[1], 255 - pixel[2]]
+    if len(imagen.shape) == 3:
+        for y in range(alto):
+            for x in range(ancho):
+                pixel = imagen[y, x]
+                # Nuevo pixel en negativo (255 - canal)
+                negativo[y, x] = [255 - pixel[0], 255 - pixel[1], 255 - pixel[2]]
+    else:  # Si la imagen es blanco y negro
+        for y in range(alto):
+            for x in range(ancho):
+                pixel = imagen[y, x]
+                # Nuevo pixel en negativo (255 - valor)
+                negativo[y, x] = 255 - pixel
     
     return negativo
 
 if __name__ == "__main__":
     image_path = "C:/Users/Hp245-User/Desktop/openCV/images/amarilla.png"
-    imagen = cv2.imread(image_path)
+    imagen = cv2.imread(image_path,0)
 
     negativo = obtener_negativo(imagen)
 
